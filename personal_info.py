2ba9a9151d803340f18626b11952ce170fbe4f64
Paulo Edu Dev <pauloedudev@gmail.com>
2024-03-18 15:43:13 -0300

    Add: Print das informações e Comentários no código

--- personal_info.py
+++ personal_info.py
@@ -1,3 +1,6 @@
 print('Insira seus dados pessoais: \n')
 nome = input('Nome: ')
 idade = int(input('Idade: '))
+
+print(f'\nSeu nome é: {nome}') #O '\n' é para melhorar a formatação no terminal
+print(f'Sua idade é: {idade}')
